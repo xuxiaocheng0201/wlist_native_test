c58c09a13950e725e2364ca020fee29abf54e0c6
xuxiaocheng <2762267080@qq.com>
2024-09-05 15:52:27 +0800

    feat(lib): 添加initialize函数

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use tokio::sync::OnceCell;
+use tokio::task::spawn_blocking;
+
 #[test]
 fn versions() {
     assert_eq!(
@@ -12,4 +15,15 @@ fn versions() {
         wlist_native::common::versions::get_web_api_version(),
         "0.2.0"
     );
+}
+
+pub async fn initialize() -> anyhow::Result<()> {
+    static INIT: OnceCell<()> = OnceCell::const_new();
+    INIT.get_or_try_init(|| async {
+        spawn_blocking(|| {
+            tracing_subscriber::fmt().with_max_level("debug").init();
+            wlist_native::common::workspace::initialize("run/data", "run/cache")?;
+            wlist_native::common::database::initialize()
+        }).await.map_err(Into::into).and_then(|r| r)
+    }).await.map(|()| ())
 }
