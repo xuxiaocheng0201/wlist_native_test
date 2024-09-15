e1fb03ba8d7fb34e0356cd9d8f69805292711f78
xuxiaocheng <2762267080@qq.com>
2024-09-15 16:38:16 +0800

    feat(lib): 优化trace过滤器

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,9 @@ use std::fmt::{Debug, Display};
 
 use tokio::sync::{OnceCell, RwLock, RwLockReadGuard, RwLockWriteGuard};
 use tokio::task::spawn_blocking;
-use tracing_subscriber::filter::LevelFilter;
+use tracing_subscriber::Layer;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
 
 #[cfg(test)]
 mod common;
@@ -22,7 +24,11 @@ pub async fn initialize(unique: bool) -> anyhow::Result<InitializeGuard> {
     static INIT: OnceCell<()> = OnceCell::const_new();
     INIT.get_or_try_init(|| async {
         spawn_blocking(|| {
-            tracing_subscriber::fmt().with_max_level(LevelFilter::DEBUG).init();
+            tracing_subscriber::registry::Registry::default().with(tracing_subscriber::fmt::layer().with_filter(
+                <tracing_subscriber::filter::Targets as std::str::FromStr>::from_str(
+                    "wlist_native_test=trace,[{e}]=error,[{err}]=error,[{error}]=error"
+                ).unwrap()
+            )).init();
             wlist_native::common::workspace::initialize("run/data", "run/cache")?;
             wlist_native::common::database::initialize()
         }).await.map_err(Into::into).and_then(|r| r)
@@ -41,7 +47,15 @@ pub fn uninitialize(guard: InitializeGuard) -> anyhow::Result<()> {
 }
 
 
-pub fn assert_error<T, E: Debug + Display + Send + Sync + 'static>(result: anyhow::Result<T>) -> anyhow::Result<Option<T>> {
+pub fn assert_error<T: Debug, E: Debug + Display + Send + Sync + 'static>(result: anyhow::Result<T>) -> anyhow::Result<()> {
+    match result {
+        Ok(t) => Err(anyhow::anyhow!("expect error but returned ok: {t:?}")),
+        Err(e) if e.downcast_ref::<E>().is_some() => Ok(()),
+        Err(e) => Err(e),
+    }
+}
+
+pub fn may_error<T, E: Display + Debug + Send + Sync + 'static>(result: anyhow::Result<T>) -> anyhow::Result<Option<T>> {
     match result {
         Ok(t) => Ok(Some(t)),
         Err(e) if e.downcast_ref::<E>().is_some() => Ok(None),
