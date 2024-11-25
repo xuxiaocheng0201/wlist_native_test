f17d65fdc676a026747da437f7c446243b5a14ea
xuxiaocheng <2762267080@qq.com>
2024-11-25 19:42:26 +0800

    feat(lib): 更新files_get接口

--- src/core/client/get.rs
+++ src/core/client/get.rs
@@ -9,13 +9,13 @@ pub async fn test_none(guard: &InitializeGuard) -> anyhow::Result<()> {
     let file = FileLocation { storage: 0, file_id: 0, is_directory: true, };
     let directory = FileLocation { storage: 0, file_id: 0, is_directory: false, };
 
-    let result = files_get(c!(guard), file, false).await;
+    let result = files_get(c!(guard), file, true, false).await;
     crate::assert_error::<_, wlist_native::common::exceptions::StorageNotFoundError>(result)?;
-    let result = files_get(c!(guard), directory, false).await;
+    let result = files_get(c!(guard), directory, true, false).await;
     crate::assert_error::<_, wlist_native::common::exceptions::StorageNotFoundError>(result)?;
-    let result = files_get(c!(guard), file, true).await;
+    let result = files_get(c!(guard), file, true, true).await;
     crate::assert_error::<_, wlist_native::common::exceptions::StorageNotFoundError>(result)?;
-    let result = files_get(c!(guard), directory, true).await;
+    let result = files_get(c!(guard), directory, true, true).await;
     crate::assert_error::<_, wlist_native::common::exceptions::StorageNotFoundError>(result)?;
 
     Ok(())
@@ -24,7 +24,7 @@ pub async fn test_none(guard: &InitializeGuard) -> anyhow::Result<()> {
 #[allow(dead_code)]
 pub async fn get(guard: &InitializeGuard, parent: Option<i64>, location: FileLocation, check: bool) -> anyhow::Result<FileDetailsInformation> {
     loop {
-        let error = match files_get(c!(guard), location, check).await {
+        let error = match files_get(c!(guard), location, false, check).await {
             Ok(information) => break Ok(information), Err(error) => error,
         };
         if error.downcast_ref::<wlist_native::common::exceptions::FileNotFoundError>().is_some() {
@@ -56,28 +56,28 @@ pub async fn close_thumbnail(guard: &InitializeGuard, information: &FileDetailsI
 }
 
 pub async fn test_normal(guard: &InitializeGuard, root: FileLocation) -> anyhow::Result<()> {
-    let information = files_get(c!(guard), root, false).await?;
+    let information = files_get(c!(guard), root, false, false).await?;
     assert_eq!(information.path, Vec::<String>::new());
     close_thumbnail(guard, &information).await?;
 
     let list = super::list::list(guard, root, None).await?;
 
     let location = FileLocation { storage: root.storage, file_id: list.files[0].id, is_directory: false, };
-    let information = files_get(c!(guard), location, false).await?;
+    let information = files_get(c!(guard), location, false, false).await?;
     assert_eq!(information.basic.name.as_str(), "chunk.txt");
     assert_md5(Some("fc6cb96d6681a62e22a2bbd32e5e0519"), &information);
     assert_eq!(information.path, Vec::<String>::new());
     close_thumbnail(guard, &information).await?;
 
     let location = FileLocation { storage: root.storage, file_id: list.files[3].id, is_directory: false, };
-    let information = files_get(c!(guard), location, false).await?;
+    let information = files_get(c!(guard), location, false, false).await?;
     assert_eq!(information.basic.name.as_str(), "large.txt");
     assert_md5(Some("99f7ad3d42ac3318dcc92b64beecb179"), &information);
     assert_eq!(information.path, Vec::<String>::new());
     close_thumbnail(guard, &information).await?;
 
     let location = FileLocation { storage: root.storage, file_id: list.files[2].id, is_directory: true, };
-    let information = files_get(c!(guard), location, false).await?;
+    let information = files_get(c!(guard), location, false, false).await?;
     assert_eq!(information.basic.name.as_str(), "hello");
     assert_md5(None, &information);
     assert_eq!(information.path, Vec::<String>::new());
@@ -85,7 +85,7 @@ pub async fn test_normal(guard: &InitializeGuard, root: FileLocation) -> anyhow:
 
     let hello = super::list::list(guard, location, None).await?;
     let location = FileLocation { storage: root.storage, file_id: hello.files[0].id, is_directory: false, };
-    let information = files_get(c!(guard), location, false).await?;
+    let information = files_get(c!(guard), location, false, false).await?;
     assert_eq!(information.basic.name.as_str(), "hello.txt");
     assert_md5(Some("fc3ff98e8c6a0d3087d515c0473f8677"), &information);
     assert_eq!(information.path, vec!["hello".to_string()]);
@@ -95,7 +95,7 @@ pub async fn test_normal(guard: &InitializeGuard, root: FileLocation) -> anyhow:
 }
 
 pub async fn test_empty(guard: &InitializeGuard, root: FileLocation) -> anyhow::Result<()> {
-    let information = files_get(c!(guard), root, false).await?;
+    let information = files_get(c!(guard), root, false, false).await?;
     assert_eq!(information.basic.id, information.basic.parent_id);
     close_thumbnail(guard, &information).await?;
     Ok(())
